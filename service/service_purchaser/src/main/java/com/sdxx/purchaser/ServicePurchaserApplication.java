582c9c8dc6a15ac66004584fc8159dd4eb2c3f9d
PdeKingsley <103254672+PdeKingsley@users.noreply.github.com>
2022-06-14 14:04:28 +0800

    .

--- service/service_purchaser/src/main/java/com/sdxx/purchaser/ServicePurchaserApplication.java
+++ service/service_purchaser/src/main/java/com/sdxx/purchaser/ServicePurchaserApplication.java
@@ -1,11 +1,9 @@
 package com.sdxx.purchaser;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication
 public class ServicePurchaserApplication {
 
 	public static void main(String[] args) {
