b0ab20568049d1d65a96ddeace436f306084f703
zhangjie <zhangjie@163.com>
2022-06-14 16:11:08 +0800

    删除

--- service/service_purchaser/src/main/java/com/sdxx/purchaser/mapper/AdminContextMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sdxx.purchaser.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sdxx.purchaser.entity.AdminContext;
-
-public interface AdminContextMapper extends BaseMapper<AdminContext> {
-}
--- service/service_purchaser/src/main/java/com/sdxx/purchaser/mapper/ContextMenuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sdxx.purchaser.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sdxx.purchaser.entity.ContextMenu;
-
-public interface ContextMenuMapper extends BaseMapper<ContextMenu> {
-}
